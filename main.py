1cf66cf495409c236c20e0d43e470dfa8a639ef6
qiaoshuixia521 <1580542048@qq.com>
2021-11-10 17:09:06 +0800

    本地修改1

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 # execute(['scrapy','crawl','shij'])
 # execute(['scrapy','crawl','zhihu'])
 execute(["scrapy","crawl","taobao"])
+
+
+#本地修改1
